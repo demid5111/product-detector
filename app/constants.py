0d056cdee4ed50531eb248ef495f60420fac0645
Alexander Demidovskij <monadv@yandex.ru>
2023-06-25 18:20:59 +0300

    basic POC

---
+++ app/constants.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+STATIC_ASSETS_PATH = Path(__file__).parent / 'static'
+DATA_PATH = Path(__file__).parent.parent / 'data'
+DATA_UPLOADS_PATH = DATA_PATH / 'uploads'
+DATA_MODELS_PATH = DATA_PATH / 'models'
+DATA_DETECTIONS_PATH = DATA_PATH / 'detections'
